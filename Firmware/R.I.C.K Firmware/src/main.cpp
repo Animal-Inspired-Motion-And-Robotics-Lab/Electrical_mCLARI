e77f1edfd1645f7c36267786bc449381ddfdb517
Parker McDonnell <wimc6189@colorado.edu>
2023-09-04 16:52:05 -0600

    Added code to test joystick and GPIO expander

--- Firmware/R.I.C.K Firmware/src/main.cpp
+++ Firmware/R.I.C.K Firmware/src/main.cpp
@@ -1,18 +1,46 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+
+// libraries
+
+#include "MCP23008.h"
+#include "Arduino_ST7789.h"
+
+MCP23008 MCP(0x24);
+
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+
+  Wire.begin();
+  MCP.begin();
+
+  Serial.print("MCP23008_test version: ");
+  Serial.println(MCP23008_LIB_VERSION);
+
+  Serial.print("Connect: ");
+  Serial.println(MCP.isConnected());
+
+
+  for (int pin = 0; pin < 8; pin++)
+  {
+    MCP.pinMode(pin, INPUT);
+  }
+  Serial.println("TEST digitalRead(pin)");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  for (int pin = 0; pin < 8; pin++)
+  {
+    int val = MCP.digitalRead(pin);
+    Serial.print(val);
+    Serial.print('\t');
+  }
+  Serial.println();
+  delay(100);
+
 }
