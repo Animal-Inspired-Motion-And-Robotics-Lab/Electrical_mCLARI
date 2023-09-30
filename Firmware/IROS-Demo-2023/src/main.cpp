1dd37e8a79529333c373126bb7f13bdcdfe42823
Parker McDonnell <wimc6189@colorado.edu>
2023-09-29 23:56:09 -0600

    Started IROS demo code
    Got all GPIO expanders working, power monitor, can enable HV board load switches and turn on the HV boost converter and enable HV amplifiers 1-8. Tomorrow I will control the ADC and DACs and get some waveform generation code going.

---
+++ Firmware/IROS-Demo-2023/src/main.cpp
@@ -0,0 +1,228 @@
+#include <Arduino.h>
+#include <Adafruit_MCP23X17.h>
+#include <Adafruit_NeoPixel.h>
+#include <Wire.h>
+#include <SPI.h>
+
+// put function declarations here:
+
+//-------------------------------------------------------------------------------------------
+//                                       INCLUDES
+//-------------------------------------------------------------------------------------------
+
+#include "pcb-v1.h"
+
+// NeoPixel test program showing use of the WHITE channel for RGBW
+// pixels only (won't look correct on regular RGB NeoPixel strips).
+
+Adafruit_MCP23X17 mcp_top;
+Adafruit_MCP23X17 mcp_bottom;
+Adafruit_MCP23X17 mcp_hv;
+
+#define WIRE Wire
+
+// Which pin on the Arduino is connected to the NeoPixels?
+// On a Trinket or Gemma we suggest changing this to 1:
+#define LED_PIN MCU_NEOPIXEL  
+
+// How many NeoPixels are attached to the Arduino?
+#define LED_COUNT  26
+
+// NeoPixel brightness, 0 (min) to 255 (max)
+#define BRIGHTNESS 50 // Set BRIGHTNESS to about 1/5 (max = 255)
+
+// Declare our NeoPixel strip object:
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_RGB + NEO_KHZ800);
+// Argument 1 = Number of pixels in NeoPixel strip
+// Argument 2 = Arduino pin number (most are valid)
+// Argument 3 = Pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+
+// Fill strip pixels one after another with a color. Strip is NOT cleared
+// first; anything there will be covered pixel by pixel. Pass in color
+// (as a single 'packed' 32-bit value, which you can get by calling
+// strip.Color(red, green, blue) as shown in the loop() function above),
+// and a delay time (in milliseconds) between pixels.
+// Fill the dots one after the other with a color
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
+
+//Theatre-style crawling lights.
+void theaterChase(uint32_t c, uint8_t wait) {
+  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
+    for (int q=0; q < 3; q++) {
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, c);    //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+//Theatre-style crawling lights with rainbow effect
+void theaterChaseRainbow(uint8_t wait) {
+  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
+    for (int q=0; q < 3; q++) {
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
+      }
+      strip.show();
+
+      delay(wait);
+
+      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+      }
+    }
+  }
+}
+
+
+
+
+void setup() {
+  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
+  #if defined (__AVR_ATtiny85__)
+    if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+  #endif
+  // End of trinket special code
+
+  WIRE.begin();
+  SPI.begin();
+  strip.begin();
+  strip.setBrightness(50);
+  strip.show(); // Initialize all pixels to 'off'
+
+  Serial.begin(9600);
+  Serial.println("MCP23xxx Combo Test!");
+
+  // initialize the MCP23017 pin expander on all boards
+  mcp_bottom.begin_I2C(GPIO_BOTTOM_ADDR);
+  mcp_top.begin_I2C(GPIO_TOP_ADDR);
+  mcp_hv.begin_I2C(HV_GPIO_ADDR);
+
+  // turn on the high voltage board 
+  mcp_bottom.pinMode(BRD_ENABLE_1, OUTPUT);
+  mcp_bottom.digitalWrite(BRD_ENABLE_1, HIGH);
+  // enable the power monitor on the PCB V1
+  mcp_bottom.pinMode(PWR_SHDN_L, OUTPUT);
+  mcp_bottom.digitalWrite(PWR_SHDN_L, HIGH);
+
+  // enable the HV boost converter charge pump
+  mcp_hv.pinMode(HV_CP_EN_0, OUTPUT);
+  mcp_hv.digitalWrite(HV_CP_EN_0, HIGH);
+
+  // enable the HV boost converter
+  mcp_hv.pinMode(HV_CS_0, OUTPUT);
+  mcp_hv.digitalWrite(HV_CS_0, LOW);
+  SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE0));
+  //SPI.transfer16(0x00F4);
+  SPI.transfer16(0x00E4);
+  SPI.endTransaction();
+  mcp_hv.digitalWrite(HV_CS_0, HIGH);
+
+}
+
+void loop() {
+
+  rainbow(20);
+  rainbowCycle(20);
+
+  byte error, address;
+  int nDevices;
+
+  Serial.println("Scanning...");
+
+  nDevices = 0;
+  for(address = 1; address < 127; address++ ) 
+  {
+    // The i2c_scanner uses the return value of
+    // the Write.endTransmisstion to see if
+    // a device did acknowledge to the address.
+    WIRE.beginTransmission(address);
+    error = WIRE.endTransmission();
+
+    if (error == 0)
+    {
+      Serial.print("I2C device found at address 0x");
+      if (address<16) 
+        Serial.print("0");
+      Serial.print(address,HEX);
+      Serial.println("  !");
+
+      nDevices++;
+    }
+    else if (error==4) 
+    {
+      Serial.print("Unknown error at address 0x");
+      if (address<16) 
+        Serial.print("0");
+      Serial.println(address,HEX);
+    }    
+  }
+  if (nDevices == 0)
+    Serial.println("No I2C devices found\n");
+  else
+    Serial.println("done\n");
+
+  delay(5000);           // wait 5 seconds for next scan
+
+}
+
+
---
+++ Firmware/IROS-Demo-2023/src/pcb-v1.h
@@ -0,0 +1,94 @@
+/*------------------------------------------------------------------------------------------
+*File:      pcb-v1.h
+*Author:    Parker McDonnell
+*Lab:       AIM Robotics Lab
+*Company:   University of Colorado Boulder
+*
+*-------------------------------------------------------------------------------------------*/
+
+#ifndef PCB_V1_H_
+#define PROPRIOCEPTOR_H_
+
+// MCU pin definitions
+
+#define MCU_1_WIRE          A0
+#define MCU_GPIO0_INT       A1
+#define MCU_GPIO1_INT       A2   // MCU_SPARE_1 -- PCB different than Proprioceptor v1.2
+#define MCU_NEOPIXEL        A3   // MCU_SPARE_0
+#define MCU_RX              RX
+#define MCU_TX              TX
+#define MCU_I2C_SCL         SCL
+#define MCU_I2C_SDA         SDA
+#define MCU_SPI_MOSI        MOSI
+#define MCU_SPI_MISO        MISO
+#define MCU_SPI_SCK         SCK
+
+// GPIO Bottom pin definitions
+
+#define PWR_SHDN_L          0
+#define SD_CS               1
+#define PWR_ALERT1          2
+#define PWR_ALERT2          3
+#define CARD_INSERT         4
+#define CHG_EN              5
+#define BATT_STAT           6
+#define DAC_CS              7
+#define GPIO_BOTTOM_8       8
+#define GPIO_BOTTOM_9       9
+#define GPIO_BOTTOM_10      10
+#define BRD_ENABLE_1        10
+#define GPIO_11             11
+#define GPIO_12             12
+#define GPIO_13             13
+#define RESET               14
+#define AUDIO_SD            15
+
+// GPIO Top pin definitions
+
+#define LCD_LIGHT           0
+#define LCD_DC              1
+#define LCD_RESET           2
+#define LCD_CS              3
+#define MEM_CS              4
+#define TS_CS               5
+#define LCD_INT             6
+#define LCD_BUSY            7
+#define LCD_GPIO_1          8
+#define LCD_GPIO_2          9
+#define GPIO_SPARE1         10
+#define GPIO_SPARE2         11
+#define USB_ALERT           12
+#define USB_AB_SIDE         13
+#define USB_ATTACH          14
+#define USB_RESET           15
+
+// GPIO pin definitions
+
+#define HV_ADC_CS              0
+#define HV_PWR_ALERT2          1
+#define HV_GPIO_SPARE_0        2
+#define HV_GPIO_SPARE_1        3
+#define HV_EXT_HV_STAT         4
+#define HV_ROBOT_CS            5
+#define HV_ADH_EN_0            6
+#define HV_ADH_EN_1            7
+#define HV_ADH_EN_2            8
+#define HV_ADC_EN_3            9
+#define HV_CS_0                10
+#define HV_CP_EN_0             11
+#define HV_DAC_CS_0            12
+#define HV_BRD_EN              13
+#define HV_PWR_SHDN_L          14
+#define HV_PWR_ALERT1          15
+
+// HV board devices
+#define HV_GPIO_ADDR        0x23
+#define HV_PWR_MONITOR_ADDR 0x48
+
+#define GPIO_TOP_ADDR       0x26
+#define GPIO_BOTTOM_ADDR    0x27
+#define ADC_ADDR            0x48
+#define PWR_MONITOR_ADDR    0x48
+
+
+#endif /* PCB_V1_H_ */
